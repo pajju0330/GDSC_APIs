7868f8d83f158c1dbe724a9b313c47f062f05f67
pajju <prajwalshah.2003@gmail.com>
2023-02-12 13:45:21 +0530

    adding cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const router = require('./routers/movieRouter');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const router = require('./routers/movieRouter');
+
+app.use(cors());
 app.use(express.json());
 app.use('/api/v1/movie', router);
 
